ef0162b2adcae553c84dd989921a16c4205bd747
Tan Sein_Jone <seinjonetan@gmail.com>
2024-02-09 15:14:51 -0800

    feat: -Add time delays and check status before and after requesing pass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -10,10 +11,10 @@ def parse_yml():
 	Parses the config file and returns dict
 	"""
 	with open('config.yml') as stream:
-	    try:
-	        config = yaml.safe_load(stream)
-	    except yaml.YAMLError as exc:
-	        print(exc)
+		try:
+			config = yaml.safe_load(stream)
+		except yaml.YAMLError as exc:
+			print(exc)
 	return config
 
 def init_driver():
@@ -32,7 +33,7 @@ def upass_loadpage(config, driver):
 	driver.get("https://upassbc.translink.ca/")
 	option_visible_text = config["university"]
 	select = driver.find_element(by="id", value="PsiId")
-	driver.execute_script("var select = arguments[0]; for(var i = 0; i < select.options.length; i++){ if(select.options[i].text == arguments[1]){ select.options[i].selected = true; } }", select, option_visible_text);
+	driver.execute_script("var select = arguments[0]; for(var i = 0; i < select.options.length; i++){ if(select.options[i].text == arguments[1]){ select.options[i].selected = true; } }", select, option_visible_text)
 
 	driver.find_element(by="name", value="PsiId").send_keys(Keys.RETURN)
 	driver.find_element(by="name", value="PsiId").send_keys(Keys.RETURN)
@@ -64,8 +65,20 @@ def sfu_auth(config, driver):
 
 def request_pass(driver):
 
-	driver.find_element(by="id", value="chk_1").click()
-	driver.find_element(by="id", value="requestButton").click()
+	try:
+		driver.find_element(by="id", value="chk_1").click()
+		driver.find_element(by="id", value="requestButton").click()
+	except:
+		print("No pass available for request.")
+	
+def check_stats(driver):
+	# Check status to ensure that the pass has been requested
+	status_divs = driver.find_elements(by="class name", value="status")
+	for div in status_divs:
+		if "Processed" not in div.text and "Requested" not in div.text:
+			print("Action was not successful")
+			return
+	print("Action was successful")
 
 
 def main():
@@ -85,7 +98,15 @@ def main():
 		print("Sorry, your university is not currently supported.")
 
 
+	# Wait for 10 seconds
+	time.sleep(10)
+
 	request_pass(driver)
+
+	# Wait for 10 seconds 
+	time.sleep(10)
+
+	check_stats(driver)
 
 
 if __name__=="__main__":
